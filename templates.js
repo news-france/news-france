e173e9edeb5b8ee133cf8d5116a5ffaba4ac3b00
Robin <robin.perice@protonmail.com>
2023-11-08 17:20:24 +0100

    Update templates.js

--- templates.js
+++ templates.js
@@ -5,8 +5,8 @@ module.exports.document = function (body) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Extrait de la section Toulouse de Google Actualités">
-    <title>Actualités Toulouse</title>
+    <meta name="description" content="Extrait de la section France de Google Actualités">
+    <title>Actualités France</title>
     <link type="text/css" rel="stylesheet" href="./styles.css" media="all">
   </head>
   <body>
