f17332a86062efec691d6e334f14cac54dca370d
Robin <codeberg.otmyj@passmail.net>
2023-12-05 22:33:31 +0100

    switch clean_url to private-reader

--- scripts/clean_url.sh
+++ scripts/clean_url.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 REDIRECTED_URL=$(curl -w "%{url_effective}\n" -I -L -s -S $1 -o /dev/null)
-CLEAN_URL=https://clearthis.page/?u=${REDIRECTED_URL}
+CLEAN_URL=https://private-reader.nogafam.fr/${REDIRECTED_URL}
 echo $CLEAN_URL
